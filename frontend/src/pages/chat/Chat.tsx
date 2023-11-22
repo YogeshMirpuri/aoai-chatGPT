5bd60c225077ffbf869697a02e2725a2e7f0864b
YogeshMirpuri <120175411+YogeshMirpuri@users.noreply.github.com>
2023-11-22 18:37:37 +0530

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -9,7 +9,7 @@ import uuid from 'react-uuid';
 import { isEmpty } from "lodash-es";
 
 import styles from "./Chat.module.css";
-import Azure from "../../assets/Azure.svg";
+import BPAI from "../../assets/BPAI.svg";
 
 import {
     ChatMessage,
